803a1f67564ea30330ddb561c107ce4497da8b68
vivienbarchet <vivien.barchet@gmx.de>
2026-07-15 10:16:43 -0600

    new analysis

--- analysis/02_fit_model.py
+++ analysis/02_fit_model.py
@@ -27,7 +27,7 @@ print("PyMC", pm.__version__, "| ArviZ", az.__version__)
 # ## 0. Config
 
 # %%
-SUBJECT = "sub04"
+SUBJECT = "sub07"
 ALIGNMENT_CSV = f"../save/derived/{SUBJECT}/{SUBJECT}_click_tap_alignment.csv"
 OUT_DIR = f"../save/derived/{SUBJECT}"
 
